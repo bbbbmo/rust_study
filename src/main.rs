f3d99ec50783d312065f288f798e808128a1e3bf
bbbbmo <bbbbmo@github.com>
2025-11-07 13:50:55 +0900

    ✨ feat(io): 추리 게임 입력 기능 추가

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io; // 입출력(io)에 관련된 기능들을 제공하는 라이브러리 불러오기
+
+fn main() { // main 함수 선언 - 프로그램에서 가장 먼저 시작되는 특별한 함수
+    println!("Guess the number!"); // println!은 문자열을 화면에 출력하는 매크로(!가 붙으면 매크로임)
+
+    println!("추측되는 번호를 입력해주세요");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Faild to read line");
+
+    println!("입력한 추측 값: {guess}");
 }
