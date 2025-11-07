91c1b364480c7f17f7c28ad8954245431c9d8630
bbbbmo <bbbbmo@github.com>
2025-11-07 15:27:50 +0900

    🎨 refactor(guessing_game): 추리 게임 함수 분리

---
+++ src/guessing_game/mod.rs
@@ -0,0 +1,5 @@
+pub mod guessing_game;
+
+// 함수를 직접 내보내서 더 간단하게 사용할 수 있도록 함
+pub use guessing_game::guessing_game;
+
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,5 @@
-use std::{cmp::Ordering, io}; // 입출력(io)에 관련된 기능들을 제공하는 라이브러리 불러오기
-use rand::Rng; // Rng는 난수 생성기를 구현한 메서드들을 정의한 트레이트 (trait)
+mod guessing_game; // guessing_game 모듈 선언
 
 fn main() { 
-    println!("Guess the number!"); 
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("시크릿 번호: {secret_number}");
-
-    loop {
-        println!("추측되는 번호를 입력해주세요");
-    
-        let mut guess = String::new(); // 빈 문자열을 초기값으로 가진 가변 변수 생성
-    
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Faild to read line");
-    
-        let guess: u32 = guess.trim().parse().expect("타입은 number여야 합니다");
-          
-        println!("입력한 추측 값: {guess}");
-    
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too Big!"),
-            Ordering::Equal => {
-                println!("You Win!"); 
-                break;
-            },
-        }
-    }
+    guessing_game::guessing_game(); // 함수 호출 (pub use로 간단하게)
 }
